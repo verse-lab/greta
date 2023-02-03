c633972eb05f32ab7af19117facac9e885caf2d7
yunjeong-lee <yunjeong.lee@u.nus.edu>
2023-02-03 10:42:27 +0800

    Revised cross products of two transition lists (work in progress)

--- bin/main.ml
+++ bin/main.ml
@@ -3,25 +3,28 @@ open Stdlib
 
 module P = Parser
 module L = Lexer
+module T = Ta
 module C = Converter
 module E = Examples
 module Lr = Learner
-module T = Ta
 module R = Run
 module O = Operation
 
 let () =
+  let debug_print = true in
   let parser_file = "./lib/parser.mly" in
   let conflicts_file = "../_build/default/lib/parser.conflicts" in
-  let debug_print = true in
-  let ta_origin:T.ta = C.convertToTa parser_file in
-  let ranked_symbols:T.symbol list = ta_origin.alphabet in
+  let ta_initial:T.ta = C.convertToTa parser_file in
+  let ranked_symbols:T.symbol list = ta_initial.alphabet in
+  (* TODO: replace example_tree with multiple examples based on conflics_file *)
   let example_tree:T.tree = E.ex03 in
   let _ (* examples *):T.tree list = E.gen_examples conflicts_file in 
+  (* TODO: run learner -> /\ -> normalize -> ta to cfg -> overwrite parser 
+     until all conflicts disappear (idea: connect with example generation) *)
   let ta_learned:T.ta = Lr.learner example_tree ranked_symbols in
   let _:T.tree = E.rand_tree ranked_symbols debug_print 0 in
   let _:bool = R.accept ta_learned example_tree debug_print in
-  let _:T.ta = O.intersect ta_origin ta_learned debug_print in
+  let _:T.ta = O.intersect ta_initial ta_learned debug_print in
   while true do
     let inp = read_line () in
     match Utils.parse_string inp with
--- lib/operation.ml
+++ lib/operation.ml
@@ -14,27 +14,42 @@ let cartesian_product_states (ls1: state list) (ls2: state list): state list =
       (* remove duplicate ϵ states *)
   in loop ls1 ls2 [] |> remove_dups |> List.rev
 
-(* let cartesian_product_trans (trans1: transition list) (trans2: transition list): transition list =
-  let rec loop l1 l2 acc =
-    match l1, l2 with
-    | [], _ | _, [] -> acc
-    | (st1, (sym1, stls1)) as h1 :: tl1, (st2, (sym2, stls2)) :: tl2 ->
-      let acc' = loop [h1] tl2 ((h1^h2)::acc) 
-      in loop tl1 ls2 (acc' @ acc)
-  in loop trans1 trans2 [] *)
+let cartesian_product_trans (states1: state list) (states2: state list) 
+(trans1: transition list) (trans2: transition list) (syms: symbol list): transition list =
+  let open List in
+  let remove_epsilon ls = filter (fun x -> not (x = "ϵ")) ls in
+  (** find_rhs_states : based on lhs_state and sym, find corresonding 'Some rhs_states'
+     and return 'None' if it doesn't find corresponding list *)
+  let find_rhs_states _ _ _ (* lhs_state sym trans *): state list option = 
+    Some []
+  in
+  let stats1, stats2 = remove_epsilon states1, remove_epsilon states2 in
+  (* TODO: simplify code below *)
+  let states_pairs: (state * state) list = combine stats1 stats2 in
+  let states_lhs = cartesian_product_states stats1 stats2 in
+  let states_tuples: (state * (state * state)) list = combine states_lhs states_pairs in 
+  let res_trans: transition list = map2 (fun (lhs, (stat1, stat2)) sym ->
+    match find_rhs_states stat1 sym trans1, find_rhs_states stat2 sym trans2 with 
+    | Some rhs_states1, Some rhs_states2 -> 
+      let rhs_states = cartesian_product_states rhs_states1 rhs_states2 in 
+      (lhs, (sym, rhs_states))
+    | Some _(*v1*), None ->  (lhs, (sym, []))
+    | None, Some _(*v2*) -> (lhs, (sym, []))
+    | None, None -> raise (Invalid_argument "No transition available!")
+    ) states_tuples syms in res_trans
 
-(** Intersection of tree automata:  *)
+(** Intersection of tree automata *)
 let intersect (a1: ta) (a2: ta) (debug_print: bool): ta =
   let open Printf in
-  if debug_print then (printf "\nIntersect the following 2 TAs:\n\tFirst TA:\n";
-  Pp.pp_ta a1; printf "\n\tSecond TA:\n"; Pp.pp_ta a2; printf "\n");
+  if debug_print then (printf "\nIntersect the following 2 TAs:\n  First TA:\n";
+  Pp.pp_ta a1; printf "\n  Second TA:\n"; Pp.pp_ta a2; printf "\n");
   (* TODO: Add a sanity check on alphabet based on set equality *)
   let statesls = cartesian_product_states a1.states a2.states in
   let start = cartesian_product_states [a1.start_state] [a2.start_state] |> List.hd in
   let trans = [] in
   let res_ta = { states = statesls ; alphabet = a1.alphabet
   ; start_state = start ; transitions = trans } in
-  if debug_print then (printf "\nResult of intersection: \n"; Pp.pp_ta res_ta); 
+  if debug_print then (printf "\nResult of TA intersection: \n"; Pp.pp_ta res_ta); 
   res_ta
 
 
