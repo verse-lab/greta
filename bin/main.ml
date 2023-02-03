b27bc191789feee7b474aff7875b355d5c54f35e
yunjeong-lee <yunjeong.lee@u.nus.edu>
2023-02-03 09:35:34 +0800

    Cross product with ϵ state should be ϵ

--- bin/main.ml
+++ bin/main.ml
@@ -8,17 +8,20 @@ module E = Examples
 module Lr = Learner
 module T = Ta
 module R = Run
+module O = Operation
 
 let () =
   let parser_file = "./lib/parser.mly" in
   let conflicts_file = "../_build/default/lib/parser.conflicts" in
+  let debug_print = true in
   let ta_origin:T.ta = C.convertToTa parser_file in
   let ranked_symbols:T.symbol list = ta_origin.alphabet in
   let example_tree:T.tree = E.ex03 in
   let _ (* examples *):T.tree list = E.gen_examples conflicts_file in 
-  let learned_ta:T.ta = Lr.learner example_tree ranked_symbols in
-  let _:T.tree = E.rand_tree ranked_symbols true 0 in
-  let _:bool = R.accept learned_ta example_tree true in
+  let ta_learned:T.ta = Lr.learner example_tree ranked_symbols in
+  let _:T.tree = E.rand_tree ranked_symbols debug_print 0 in
+  let _:bool = R.accept ta_learned example_tree debug_print in
+  let _:T.ta = O.intersect ta_origin ta_learned debug_print in
   while true do
     let inp = read_line () in
     match Utils.parse_string inp with
--- lib/operation.ml
+++ lib/operation.ml
@@ -1,22 +1,41 @@
 open Ta
 
 let cartesian_product_states (ls1: state list) (ls2: state list): state list =
+  let unique_cons elem ls = if (List.mem elem ls) then ls else elem :: ls in
+  let remove_dups ls = List.fold_right unique_cons ls [] in
   let rec loop l1 l2 acc =
     match l1, l2 with
     | [], _ | _, [] -> acc
-    | h1::tl1, h2::tl2 ->
+    | h1 :: tl1, h2 :: tl2 ->
+      (* cross product with ϵ results in ϵ *)
+      let prod = if (h1 = "ϵ" || h2 = "ϵ") then "ϵ" else (h1^h2) in
+      let acc' = loop [h1] tl2 (prod :: acc) 
+      in loop tl1 ls2 (acc' @ acc)
+      (* remove duplicate ϵ states *)
+  in loop ls1 ls2 [] |> remove_dups |> List.rev
+
+(* let cartesian_product_trans (trans1: transition list) (trans2: transition list): transition list =
+  let rec loop l1 l2 acc =
+    match l1, l2 with
+    | [], _ | _, [] -> acc
+    | (st1, (sym1, stls1)) as h1 :: tl1, (st2, (sym2, stls2)) :: tl2 ->
       let acc' = loop [h1] tl2 ((h1^h2)::acc) 
       in loop tl1 ls2 (acc' @ acc)
-  in loop ls1 ls2 []
+  in loop trans1 trans2 [] *)
 
 (** Intersection of tree automata:  *)
-let intersect (a1: ta) (a2: ta): ta =
+let intersect (a1: ta) (a2: ta) (debug_print: bool): ta =
+  let open Printf in
+  if debug_print then (printf "\nIntersect the following 2 TAs:\n\tFirst TA:\n";
+  Pp.pp_ta a1; printf "\n\tSecond TA:\n"; Pp.pp_ta a2; printf "\n");
   (* TODO: Add a sanity check on alphabet based on set equality *)
   let statesls = cartesian_product_states a1.states a2.states in
   let start = cartesian_product_states [a1.start_state] [a2.start_state] |> List.hd in
   let trans = [] in
-  { states = statesls ; alphabet = a1.alphabet
-  ; start_state = start ; transitions = trans }
+  let res_ta = { states = statesls ; alphabet = a1.alphabet
+  ; start_state = start ; transitions = trans } in
+  if debug_print then (printf "\nResult of intersection: \n"; Pp.pp_ta res_ta); 
+  res_ta
 
 
 (** TODO: Simplify the resulting tree automata via :
