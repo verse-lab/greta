f952dc39172a687a1a78b5e3a7ec93c4edb23e7e
yunjeong-lee <yunjeong.lee@u.nus.edu>
2024-09-19 14:30:14 +0800

    minor

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,18 +1,3 @@
-(** utilities for gluing togther the Menhir parser with the Sedlex lexer  *)
-(* let revised_parse (lexbuf: Sedlexing.lexbuf): Ast.t =
-  let tok () =
-    let tok = Lexer.token lexbuf in
-    let (st,ed) = Sedlexing.lexing_positions lexbuf in
-    (tok,st,ed) in
-  MenhirLib.Convert.Simplified.traditional2revised
-    Parser.program tok
-
-let parse lexbuf =
-  try revised_parse lexbuf with Parser.Error -> failwith "failed to parse" 
-
-let parse_string str =
-  parse (Sedlexing.Utf8.from_string str) *)
-
 (** utilities for reading lines from parser *)
 let read_line i = try Some (input_line i) with End_of_file -> None
 
